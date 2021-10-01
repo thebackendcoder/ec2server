596fd04bc2cc81c33054755d41229e3ff0dd821a
Kashif Kalam <Kashif.Kalam@viacom18.com>
2021-10-01 10:26:21 +0530

    the base route is working

--- express-app.js
+++ express-app.js
@@ -1,11 +1,8 @@
 const express = require('express');
-
 const app = express();
-
 app.get('/', (req, res) => {
     res.send("the base route is working ")
 })
-
 app.listen(3000, () => {
     console.log("listening at port 3000")
 })
