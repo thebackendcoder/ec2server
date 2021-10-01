21aa18bd295351f884bd7da1cc2a19a0fde6b424
Kashif Kalam <Kashif.Kalam@viacom18.com>
2021-10-01 10:38:41 +0530

    added the file

--- express-app.js
+++ express-app.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
+
 app.get('/', (req, res) => {
     res.send("the base route is working ")
 })
+
 app.listen(3000, () => {
     console.log("listening at port 3000")
 })
